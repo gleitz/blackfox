5b5aec167d69403de2310001525356ebf2da0a8d
Benjamin Gleitzman <gleitz@mit.edu>
2013-03-03 09:30:04 -0800

    Remove dependency on external download folder. Add requirements file. PEP8 cleanup.

--- download.py
+++ download.py
@@ -3,24 +3,29 @@ import urllib2
 
 from BeautifulSoup import BeautifulSoup as bs
 
+MAX_PAGES = 50
+
+
 def get_backgrounds():
     base = 'http://www.thefoxisblack.com/category/the-desktop-wallpaper-project/page/%s/'
-    for x in range(0, 40):
+    for x in range(0, MAX_PAGES):
         get_images_from_page(base % (x + 1))
+
 
 def get_images_from_page(url):
     html = fetchurl(url)
     soup = bs(html)
-    print soup
     for link in soup.findAll('a'):
         href = link['href']
         if '-1440x900' in href:
             print 'Downloading %s' % href
-            urllib.urlretrieve(href, '/Users/bgleitzman/Desktop/fox_backgrounds%s' % href[href.rfind('/'):])
+            urllib.urlretrieve(href, 'fox_backgrounds%s' % href[href.rfind('/'):])
+
 
 def fetchurl(url):
     response = urllib2.urlopen(url)
     return response.read()
+
 
 if __name__ == '__main__':
     get_backgrounds()
