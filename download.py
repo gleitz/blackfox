a661880d2a9a701071f851395bb5ae59092cb2ed
Benjamin Gleitzman <gleitz@mit.edu>
2013-10-03 22:23:59 -0700

    Update to requests

--- download.py
+++ download.py
@@ -1,12 +1,14 @@
-import urllib
-import urllib2
+import os
+import requests
 
 from BeautifulSoup import BeautifulSoup as bs
 
-MAX_PAGES = 50
-
+MAX_PAGES = 20
+SAVE_DIR = 'fox_backgrounds'
 
 def get_backgrounds():
+    if not os.path.exists(SAVE_DIR):
+        os.makedirs(SAVE_DIR)
     base = 'http://www.thefoxisblack.com/category/the-desktop-wallpaper-project/page/%s/'
     for x in range(0, MAX_PAGES):
         get_images_from_page(base % (x + 1))
@@ -19,12 +21,12 @@ def get_images_from_page(url):
         href = link['href']
         if '-1440x900' in href:
             print 'Downloading %s' % href
-            urllib.urlretrieve(href, 'fox_backgrounds%s' % href[href.rfind('/'):])
-
+            r = requests.get(href)
+            with open('fox_backgrounds%s' % href[href.rfind('/'):], 'wb') as f:
+                f.write(r.content)
 
 def fetchurl(url):
-    response = urllib2.urlopen(url)
-    return response.read()
+    return requests.get(url).text
 
 
 if __name__ == '__main__':
